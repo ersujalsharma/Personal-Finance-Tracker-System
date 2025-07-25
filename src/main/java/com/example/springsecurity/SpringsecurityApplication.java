38a00acfcfccf30fa6efe18498147801397ca63f
Sujal Sharma <62907239+ersujalsharma@users.noreply.github.com>
2025-07-26 02:11:17 +0530

    Added Spring Security Layer

---
+++ src/main/java/com/example/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,26 @@
+package com.example.springsecurity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.springsecurity.service.UserService;
+
+@SpringBootApplication
+public class SpringsecurityApplication implements CommandLineRunner {
+	@Autowired
+	UserService userService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+//		userService.createUser("testuser", "testpassword");
+	}
+
+
+}
